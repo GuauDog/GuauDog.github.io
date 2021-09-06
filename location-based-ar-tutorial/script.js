9a8fbc53324c76884cc13977b8a4d5ff38021a76
GuauDog <73758776+GuauDog@users.noreply.github.com>
2021-09-06 16:06:57 -0500

    Update script.js

--- location-based-ar-tutorial/script.js
+++ location-based-ar-tutorial/script.js
@@ -27,17 +27,21 @@ var models = [
         scale: '0.5 0.5 0.5',
         info: 'Magnemite, Lv. 5, HP 10/10',
         rotation: '0 180 0',
+        position: '0 3 0 ',
+
     },
     {
         url: './assets/articuno/scene.gltf',
         scale: '0.2 0.2 0.2',
         rotation: '0 180 0',
+        position: '0 3 0 ',
         info: 'Articuno, Lv. 80, HP 100/100',
     },
     {
         url: './assets/dragonite/scene.gltf',
         scale: '0.08 0.08 0.08',
         rotation: '0 180 0',
+        position: '0 3 0 ',
         info: 'Dragonite, Lv. 99, HP 150/150',
     },
 ];
