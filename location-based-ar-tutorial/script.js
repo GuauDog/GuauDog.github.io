fddc5373a2c4588168c369f653e2a2c3dfb3b93c
GuauDog <73758776+GuauDog@users.noreply.github.com>
2021-09-06 15:45:44 -0500

    Update script.js

--- location-based-ar-tutorial/script.js
+++ location-based-ar-tutorial/script.js
@@ -15,7 +15,7 @@ function staticLoadPlaces() {
 
                 lat:4.592425516681149,
                 lng:-74.19129607145788,
-                
+
             },
         },
     ];
@@ -24,20 +24,20 @@ function staticLoadPlaces() {
 var models = [
     {
         url: './assets/magnemite/scene.gltf',
-        scale: '0.1 0.2 0.2',
+        scale: '0.2 0.2 0.2',
         info: 'Magnemite, Lv. 5, HP 10/10',
-        rotation: '0 180 0',
+        rotation: '0 0 0',
     },
     {
         url: './assets/articuno/scene.gltf',
         scale: '0.1 0.1 0.1',
-        rotation: '0 180 0',
+        rotation: '0 0 0',
         info: 'Articuno, Lv. 80, HP 100/100',
     },
     {
         url: './assets/dragonite/scene.gltf',
-        scale: '0.01 0.01 0.01',
-        rotation: '0 180 0',
+        scale: '0.018 0.018 0.018',
+        rotation: '0 0 0',
         info: 'Dragonite, Lv. 99, HP 150/150',
     },
 ];
