98a68775fb56ec58f75cc8baed55656c67ca9f8d
GuauDog <73758776+GuauDog@users.noreply.github.com>
2021-09-07 13:50:09 -0500

    Update script.js

--- static-places/script.js
+++ static-places/script.js
@@ -35,7 +35,7 @@ var models = [
     },
     {
         url: './assets/dragonite/scene.gltf',
-        scale: '0.005 0.005 0.005',
+        scale: '0.003 0.003 0.003',
         rotation: '90 0 0',
         info: 'Dragonite, Lv. 99, HP 140/150',
     },
