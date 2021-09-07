e61cc164e910f2c048cbb18ade8165f5578dc371
GuauDog <73758776+GuauDog@users.noreply.github.com>
2021-09-07 12:27:39 -0500

    Update script.js

--- static-places/script.js
+++ static-places/script.js
@@ -1,7 +1,7 @@
 
 window.onload = () => {
     const button = document.querySelector('button[data-action="change"]');
-    button.innerText = 'a';
+    button.innerText = '-';
 
     let places = staticLoadPlaces();
     renderPlaces(places);
