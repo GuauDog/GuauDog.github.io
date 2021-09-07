7f7c22ec208b1658e254a7ec698870103a5aeb89
GuauDog <73758776+GuauDog@users.noreply.github.com>
2021-09-07 14:23:53 -0500

    Update script.js

--- static-places/script.js
+++ static-places/script.js
@@ -23,19 +23,19 @@ function staticLoadPlaces() {
 var models = [
     {
         url: './assets/magnemite/scene.gltf',
-        scale: '0.7 0.7 0.7',
+        scale: '0.5 0.5 0.5',
         info: 'Magnemite, Lv. 5, HP 10/10',
         rotation: '0 0 0',
     },
     {
         url: './assets/articuno/scene.gltf',
-        scale: '0.2 0.2 0.2',
+        scale: '0.1 0.1 0.1',
         rotation: '0 0 0',
         info: 'Articuno, Lv. 80, HP 100/100',
     },
     {
         url: './assets/dragonite/scene.gltf',
-        scale: '0.003 0.003 0.003',
+        scale: '0.001 0.001 0.001',
         rotation: '90 0 0',
         info: 'Dragonite, Lv. 99, HP 140/150',
     },
